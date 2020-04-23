c60d1af14601f25b5c74f5db171fd5e7232b3e46
Ghirisan Sebastian <ghirisansebastian@yahoo.com>
2020-04-23 18:00:38 +0300

    Initial Commit

---
+++ lab5_oop/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab5_oop
+//
+//  Created by Ghirisan Sebastian on 23/04/2020.
+//  Copyright © 2020 Ghirisan Sebastian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
